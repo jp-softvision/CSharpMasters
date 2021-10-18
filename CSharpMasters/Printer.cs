c7a0314d664dcdfa077abcf8c0fa11d4925a668e
jp-softvision <johnpaul.lazaro@softvision.com>
2021-10-18 15:24:50 +0800

    ContextualKeywords

---
+++ CSharpMasters/Printer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpMasters
+{
+    //
+    public class Printer<T> where T: class
+    { 
+        public void Print(T t)
+        {
+            Console.WriteLine(t);
+        }
+    }
+}
---
+++ CSharpMasters/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace CSharpMasters
+{
+    /// <summary>
+    /// This program is for magenic masters only
+    /// 
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //WEEK 1 - ASSIGNMENT
+            ContextualKeywords();
+
+            //WEEK 2- ASSIGNMENT
+
+        }
+
+
+        //Week 1 - Contextual Keywords
+        // I will use where keyword from generic class concept for my assignment 
+        // source - https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/where-generic-type-constraint
+
+        static void ContextualKeywords()
+        {
+
+            var printer = new Printer<string>();
+            printer.Print("Hello");
+        }
+
+
+
+
+    }   
+}
