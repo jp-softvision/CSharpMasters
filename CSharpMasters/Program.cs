abd002d5140075dd426d66cc661b6fe0fc46d44a
jp-softvision <johnpaul.lazaro@softvision.com>
2021-10-19 00:25:01 +0800

    Palindrome

--- CSharpMasters/Program.cs
+++ CSharpMasters/Program.cs
@@ -3,26 +3,22 @@
 namespace CSharpMasters
 {
     /// <summary>
-    /// This program is for magenic masters only
-    /// 
+    /// This program is for magenic masters only 
     /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
             //WEEK 1 - ASSIGNMENT
-            ContextualKeywords();
-
-            //WEEK 2- ASSIGNMENT
-
+            //AssignmentContextualKeywords();
+             
         }
 
 
         //Week 1 - Contextual Keywords
         // I will use where keyword from generic class concept for my assignment 
         // source - https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/where-generic-type-constraint
-
-        static void ContextualKeywords()
+        static void AssignmentContextualKeywords()
         {
 
             var printer = new Printer<string>();
@@ -30,7 +26,6 @@ namespace CSharpMasters
         }
 
 
-
-
+        
     }   
 }
---
+++ Palindrome/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = new string[] {  "one", "two", "three", "nOoN", 
+                                        "Madam, I'm Adam", "madam", 
+                                        "Hello", "Diowa", "lemon" , "Ten"};
+
+            GetPalindromeNames(names); 
+        }
+         
+        static void GetPalindromeNames(string[] names)
+        {
+            if(names != null)
+            {
+                foreach (var name in names)
+                {
+                    //removing the special characters
+                    var cleanName = RemoveSpecialCharacters(name);
+
+                    //process palindrome string
+                    if (isPalindrome(cleanName))
+                    {
+                        Console.WriteLine(name);
+                    }
+
+                }
+            } 
+        }
+
+        static bool isPalindrome(string cleanName)
+        {
+
+            //instantiate arrays of char
+            (var reversedcharArr, var charArr) = (new char[cleanName.Length], new char[cleanName.Length]);
+
+            //iterate through each chracters 
+            for (var i = 0; i < cleanName.Length; i++)
+            {
+                charArr[i] = Char.ToLower(cleanName[i]);
+                reversedcharArr[i] = Char.ToLower(cleanName[i]);
+            }
+
+            //reverse array
+            Array.Reverse(reversedcharArr); 
+             
+            var result = false;
+
+            //iterate and compare for equality
+            for (var i = 0; i < charArr.Length; i++)
+            {
+                return (!charArr[i].Equals(reversedcharArr[i])) ? false : true;
+            }
+
+            return result;
+
+        }
+
+
+        static string RemoveSpecialCharacters(string word)
+        {
+            var unwantedCharacters = new Char[] { ' ', '\'', '.', ',' };
+
+            foreach (char character in unwantedCharacters)
+            {
+                if (word.Contains(character))
+                {
+                    word.Replace(character.ToString(), string.Empty);
+                }
+            }
+
+            return word;
+
+        }  
+    }
+}
